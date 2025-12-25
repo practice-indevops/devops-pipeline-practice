6905c14f00b4536d9d266d0180906cd6960b04f0
Tiwari <avanish.tiwari@capgemini.com>
2025-12-25 20:01:49 +0530

    Initial Commit: hello app + validate script

---
+++ validate.py
@@ -0,0 +1,12 @@
+
+import sys
+
+def main():
+    print("Running validation...")
+    # Start with PASS; we'll break it later
+    print("Validation PASSED ✅")
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
+
